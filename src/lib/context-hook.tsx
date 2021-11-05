7723573120fd037eff2ddb071e56253070ac5074
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2021-11-05 22:02:15 +0700

    WIP:

--- src/lib/context-hook.tsx
+++ src/lib/context-hook.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 type TProviderProps = React.PropsWithChildren<{ contextName?: TContextName }>;
 type TProvider = React.ComponentType<TProviderProps>;
-type TContextName = '__GLOBAL__' | string; // equals to just "string", but I put '__GLOBAL__' here for semantic reason
+type TContextName = string;
 type TProviders = {
   [contextName: TContextName]: TProvider[];
 };
 
 const NO_PROVIDER = {} as never;
 
-const providers: TProviders = {
-  __GLOBAL__: [],
-};
+const GLOBAL_CONTEXT_NAME = '__GLOBAL__';
+
+const providers: TProviders = {};
 
 const getProviderKey = (key: TContextName | undefined): TContextName =>
-  key === undefined ? '__GLOBAL__' : key;
+  key === undefined ? GLOBAL_CONTEXT_NAME : key;
 
 const combineProviders = (providers: TProvider[]) =>
   providers.reduce((Combined, Provider) => ({ children }: TProviderProps) => {
@@ -29,23 +29,42 @@ export function toContextHook<TReturn>(
   hook: () => TReturn, // only accept non-parameter hook
   contextName?: TContextName
 ) {
+  const providerKey = getProviderKey(contextName);
+  if (providers[providerKey] === undefined) providers[providerKey] = [];
+
   const Context = React.createContext<TReturn>(NO_PROVIDER);
   const provider: TProvider = ({ children }) => {
     return <Context.Provider value={hook()}>{children}</Context.Provider>;
   };
-  
-  const providerKey = getProviderKey(contextName);
-  if (providers[providerKey] === undefined) providers[providerKey] = [];
-  provider.displayName = `ContextHookProvider.${providerKey}.${hook.name || 'NO_NAME'}`;
+  provider.displayName = `ContextHookProvider.${providerKey}.${
+    hook.name || 'Anonymous'
+  }`;
+
   providers[providerKey].push(provider); // TODO: display warning if the user tries to call the hook twice (use the hook in the way not intended)
 
-  return () => React.useContext(Context);
+  return () => {
+    const contextValue = React.useContext(Context);
+    if (contextValue === NO_PROVIDER) {
+      console.warn(
+        `[context-hook]: You might forget to wrap your ${providerKey} context around its consumers (by either ContextHookProvider or withContextHook)`
+      );
+    }
+    return contextValue;
+  };
 }
 
 export function ContextHookProvider(props: TProviderProps) {
   const providerKey = getProviderKey(props.contextName);
-  // TODO: remove displayName for Provider
-  const ContextProvider = combineProviders(providers[providerKey]); // TODO: handle exception
+
+  if (providers[providerKey] === undefined) {
+    console.warn(
+      `[context-hook]: ${providerKey} context is not created yet, have you called toContextHook anywhere?`
+    );
+    return props.children as JSX.Element;
+  }
+
+  const ContextProvider = combineProviders(providers[providerKey]);
+
   return <ContextProvider>{props.children}</ContextProvider>;
 }
 
